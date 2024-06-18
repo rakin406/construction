eb02ccf16b3b3f827cbcb326acf1ab4edf1ecf11
rakin406 <rakinhacker@gmail.com>
2024-06-18 19:57:46 +0600

    feat: draw simple ground

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2024 Rakin Rahman
 
 #include <constants.h>
+#include <world.h>
 
 #include <raylib.h>
 
@@ -11,20 +12,11 @@ int main() {
 
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())  // Detect window close button or ESC key
-    {
-        // Update
-        // TODO(rakin): Update your variables here
+    World world {};
 
-        // Draw
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20,
-                 LIGHTGRAY);
-
-        EndDrawing();
+    // Detect window close button or ESC key
+    while (!WindowShouldClose()) {
+        world.play();
     }
 
     CloseWindow();
